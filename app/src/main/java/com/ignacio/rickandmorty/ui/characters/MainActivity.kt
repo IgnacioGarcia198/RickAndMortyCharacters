f3a7ed428ebfae829dcd6cf67c795c6d41549c2e
IgnacioGarcia198 <ignaciogarcia198@gmail.com>
2024-11-10 05:48:34 +0100

    Use Hilt for dependency injection

--- app/src/main/java/com/ignacio/rickandmorty/ui/characters/MainActivity.kt
+++ app/src/main/java/com/ignacio/rickandmorty/ui/characters/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.ignacio.rickandmorty.ui.CharactersFeature
 import com.ignacio.rickandmorty.ui_common.theme.RickAndMortyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
