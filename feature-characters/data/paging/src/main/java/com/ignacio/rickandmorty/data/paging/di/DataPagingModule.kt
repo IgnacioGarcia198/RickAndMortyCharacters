121602f2fe6e83dac23526f592fa7e045c2827e3
IgnacioGarcia198 <ignaciogarcia198@gmail.com>
2024-11-14 06:44:08 +0100

    Add missing dependency injection

--- feature-characters/data/paging/src/main/java/com/ignacio/rickandmorty/data/paging/di/DataPagingModule.kt
+++ feature-characters/data/paging/src/main/java/com/ignacio/rickandmorty/data/paging/di/DataPagingModule.kt
@@ -4,6 +4,8 @@ import com.ignacio.rickandmorty.data.paging.mediator.RealCharactersMediatorFacto
 import com.ignacio.rickandmorty.data.paging.models.LocalRMCharacter
 import com.ignacio.rickandmorty.data.paging.pager.RealCharactersPagerFactory
 import com.ignacio.rickandmorty.data.paging.repository.RealRMCharactersPagingRepository
+import com.ignacio.rickandmorty.data.paging.updater.RealRemoteLocalUpdater
+import com.ignacio.rickandmorty.data.paging.updater.RemoteLocalUpdater
 import com.ignacio.rickandmorty.domain.repository.RMCharactersPagingRepository
 import com.ignacio.rickandmorty.paging.mediator.MediatorFactory
 import com.ignacio.rickandmorty.paging.pager.PagerFactory
@@ -23,4 +25,7 @@ interface DataPagingModule {
 
     @Binds
     fun bindCharactersPagerFactory(factory: RealCharactersPagerFactory): PagerFactory<Int, LocalRMCharacter>
+
+    @Binds
+    fun bindRemoteLocalUpdater(updater: RealRemoteLocalUpdater): RemoteLocalUpdater
 }
