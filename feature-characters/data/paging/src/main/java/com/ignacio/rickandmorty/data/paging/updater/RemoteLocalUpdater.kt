e8910bdf924173711d03027970509b1f875623d0
IgnacioGarcia198 <ignaciogarcia198@gmail.com>
2024-11-19 15:31:31 +0100

    Add RMCharactersMediatorTest

--- feature-characters/data/paging/src/main/java/com/ignacio/rickandmorty/data/paging/updater/RemoteLocalUpdater.kt
+++ feature-characters/data/paging/src/main/java/com/ignacio/rickandmorty/data/paging/updater/RemoteLocalUpdater.kt
@@ -1,8 +1,6 @@
 package com.ignacio.rickandmorty.data.paging.updater
 
-import com.ignacio.rickandmorty.data.paging.models.CharacterQueryCriteria
-
-interface RemoteLocalUpdater<Query: Any, Key: Any> {
+interface RemoteLocalUpdater<Query : Any, Key : Any> {
     /**
      * Returns Result.success with whether the remote service arrived to the end of its pagination
      * or Result.failure with a throwable if something failed.
